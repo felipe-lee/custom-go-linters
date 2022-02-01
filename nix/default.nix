fa8b8551a28b3ed0ea961d0aeab8069676b4c942
Felipe Lee <felipe@truss.works>
2022-02-01 21:39:13 +0000

    Add files that I had in progress
    This doesn't work, and might not all be needed.

--- nix/default.nix
+++ nix/default.nix
@@ -19,14 +19,14 @@ in buildEnv {
       url = "https://github.com/NixOS/nixpkgs/";
       ref = "refs/heads/master";
       rev = "9a0676dc4866128463c4cd83a20ad10384d6824f";
-    }) {}).go
+    }) {}).go_1_15
 
     (import (builtins.fetchGit {
       # Descriptive name to make the store path easier to identify
       name = "pre-commit-2.15.0";
       url = "https://github.com/NixOS/nixpkgs/";
-      ref = "refs/heads/master";
-      rev = "d73f18c222d3a21e536448cbecd33bba8142b2c1";
+      ref = "refs/heads/nixpkgs-unstable";
+      rev = "9a0676dc4866128463c4cd83a20ad10384d6824f";
     }) {}).pre-commit
 
   ];
